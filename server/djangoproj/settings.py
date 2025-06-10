0bad2a212eb3478c02eb98e8d9af6d6db385cb8d
rezahatami1992 <reza.hatami.1992@gmail.com>
2025-06-10 03:10:58 -0400

    url correction

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','https://rezahatami19-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://rezahatami19-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS = ['https://rezahatami19-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
