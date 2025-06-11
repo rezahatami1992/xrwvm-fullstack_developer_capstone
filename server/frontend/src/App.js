63b3dcfff6145943bb76e4fff6efda62f148526d
rezahatami1992 <reza.hatami.1992@gmail.com>
2025-06-11 02:17:38 -0400

    module 2 lab changes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // Add this import
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} /> {/* Add this line */}
     </Routes>
   );
 }
